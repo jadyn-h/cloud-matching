60267168c3cc8e3717bf79db150ddbf06e4c9ccf
jadyn-h <jadynmharrison@gmail.com>
2026-06-19 06:52:33 +0000

    editing alt img message

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function generateCards() {
         cardElement.setAttribute("data-name", card.name);
         cardElement.innerHTML = `
             <div class="front">
-                <img class="front-image" src=${card.image} alt="data-name" />
+                <img class="front-image" src=${card.image} alt="card,name" />
             </div>
             <div class="back"></div>
         `;
