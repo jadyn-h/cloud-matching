9204f5a71d77097d881d0585346a70108764d3e4
jadyn-h <jadynmharrison@gmail.com>
2026-06-19 06:38:09 +0000

    testing a fix for the images

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function generateCards() {
         cardElement.setAttribute("data-name", card.name);
         cardElement.innerHTML = `
             <div class="front">
-                <img class="front-image" src=${card.image} />
+                <img class="front-image" src=${card.image} alt="data-name" />
             </div>
             <div class="back"></div>
         `;
